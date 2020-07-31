561f200039ad0f6eaeea253d17e868954eaf572f
Irina-Bebenina <Irs.snoopy@gmail.com>
2020-07-31 18:11:04 +0300

    добавление файлов

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int balance;
+        balance = 2_000_000_000;
+        int transfer;
+        transfer = 500_000_000;
+        int total;
+        total = balance + transfer;
+        System.out.println(total);
+    }
+}
